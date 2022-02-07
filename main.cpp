b943d265546a3b585e6daeffd4d2a986582b39b8
Wataru <wataruoshima@my.smccd.edu>
2022-02-07 11:27:05 -0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv){
+	if (!(argv[1].empty)
+		cout << argv[1] <<endl;
+	else
+		cout << "hello world" << endl;
+	
+	return 0;
+
+}
